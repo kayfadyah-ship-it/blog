8942345912202f8247825496c70fb6f238c4f237
kayfadyah-ship-it <kayfadyah@gmail.com>
2026-09-19 23:09:46 +0700

    tes

--- database/migrations/2026_09_17_031113_create_projects_table.php
+++ database/migrations/2026_09_17_031113_create_projects_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,6 +14,8 @@ return new class extends Migration
             $table->id();
             $table->string('title', 255);
             $table->mediumText('description');
+            $table->string('category')->nullable();
+            $table->string('technology')->nullable();tes
             $table->timestamps();
         });
     }
